7a8b3d002eeba4910eda745d4b08d395f07aea10
David Montesdeoca <backpackerhh@gmail.com>
2014-07-31 12:27:33 +0100

    Remove fake data from app.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,30 +5,6 @@ var App = window.App = Ember.Application.create({
 
 App.applicationName = "Rock'n'Roll Call";
 
-App.dummySearchResultsArtists = [
-  {
-    id:         1,
-    name:       'Tom Waits',
-    type:       'artist',
-    enid:       'ARERLPG1187FB3BB39',
-    hotttnesss: '1'
-  },
-  {
-    id:         2,
-    name:       'Thomas Alan Waits',
-    type:       'artist',
-    enid:       'ARERLPG1187FB3BB39',
-    hotttnesss: '0.89'
-  },
-  {
-    id:         3,
-    name:       'Tom Waits w/ Keith Richards',
-    type:       'artist',
-    enid:       'ARMPVNN13CA39CF8FC',
-    hotttnesss: '0.79'
-  }
-];
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
