43c7836762a608a8a957499be2743c428083d0f6
David Montesdeoca <backpackerhh@gmail.com>
2014-08-02 11:13:32 +0100

    Use a view to format song's duration.

--- app/scripts/views/song_view.js
+++ app/scripts/views/song_view.js
@@ -1,12 +1,14 @@
 App.SongView = Em.View.extend({
   didInsertElement: function() {
     $('#song-duration').text(function() {
-      var $this = $(this);
-      var origSeconds = $this.attr('data-duration');
-      var minutes = Math.floor(origSeconds / 60);
-      var seconds = Math.floor(origSeconds % 60);
+      var origSeconds = $(this).attr('data-duration');
+      var minutes     = Math.floor(origSeconds / 60);
+      var seconds     = Math.floor(origSeconds % 60);
 
-      return minutes + ":" + seconds
+      if (minutes < 10) minutes = '0' + minutes;
+      if (seconds < 10) seconds = '0' + seconds;
+
+      return minutes + ':' + seconds
     });
   }
 });
